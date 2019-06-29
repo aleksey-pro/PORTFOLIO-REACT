744785656e0be08ec8bc13baa496adea3d8dae2c
davegahn <anxieter@gmail.com>
2019-06-29 23:12:46 +0300

    Saved config

--- env-config.js
+++ env-config.js
@@ -2,8 +2,8 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BASE_URL': prod
-    ? 'https://web-profi.site'
+    ? 'https://portfolio-nextjs-app.herokuapp.com'
     : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://web-profi.site',
+  'process.env.NAMESPACE': 'https://portfolio-nextjs-app.herokuapp.com',
   'process.env.CLIENT_ID': 'T22UAGUlBeeyWuQiFgkPyMevyqy04tNg',
 };
